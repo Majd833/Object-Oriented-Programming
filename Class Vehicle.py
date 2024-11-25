11c491c1063b79f706b22e5da078071eb0d03768
Majd833 <lina.azzam.1611@gmail.com>
2024-11-25 17:45:08 +0200

    Class Vehicle

---
+++ Class Vehicle.py
@@ -0,0 +1,7 @@
+class Vehicle:
+    def __init__(self,Max_speed,mileage):
+        self.Max_speed=Max_speed
+        self.mileage=mileage
+modelX=Vehicle(180,18)
+print("The modelX's max speed is:",modelX.Max_speed)
+print("The modelX's mileage is:",modelX.mileage)
